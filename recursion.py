d98c9111b488912bdca11c9873872dfbedfa3de1
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-01-09 11:54:22 +0530

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,26 @@
+def search(Diractory, sr_fname, prepath = []):
+	for key, value in Diractory.items():
+		path = prepath + ["/"] + [key]
+		if key == sr_fname or value == sr_fname:
+			return path
+		elif hasattr(value, 'items'):
+			p = search(value, sr_fname, path)
+			if p is not None:
+				return p
+	
+	
+
+Diractory = {
+	'Documents': {
+					'work': {'report.docx': None, 'file.exe': None, 'python.py': None},
+					'personal': {'Vacation.png': None, 'birthday.png': None, 'folder.zip': None} 
+					},
+	'Downloads': {'wallpaper.png': None, 'Xender.exe': None, "song.mp3": None},
+	'Songs': {
+				'Favourite songs': {'mashup2016.mp3': None, 'Tum_Hi_Hona.mp4': None, 'Galliyan.mp4': None, 'Ranjha.mp4': None},
+				'All time Favourite': {'Bahara.mp4': None, 'Main_agar_kahu.mp4': None, 'Rattan_Lambiyan.mp4': None, 'Pee_Loon.mp4': None}}
+}
+
+sr_fname = str(input("Enter a file name that you want to search : "))
+print(f"File {sr_fname} is found on this location : ",end='')
+print(*search(Diractory, sr_fname))
