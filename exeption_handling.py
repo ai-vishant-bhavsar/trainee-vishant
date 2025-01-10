1e813385314a4dedb98cd56a28698e9f026f148a
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-01-10 18:00:16 +0530

    Update exeption_handling.py

--- exeption_handling.py
+++ exeption_handling.py
@@ -111,7 +111,7 @@ while (True):
 	elif opt1 == 2:
 		while (True):
 			state_cityDict = {key: value for key in states for value in cities}
-			myDict = {countries[i] : {key:value} for i, (key,value) in enamurate(state_cityDict.items())}
+			myDict = {countries[i] : {key:value} for i, (key,value) in enumurate(state_cityDict.items())}
 			print(myDict)
 			print('''
 	1. Do you want to Update countries
@@ -133,7 +133,7 @@ while (True):
 	elif opt1 == 3:
 		while (True):
 			state_cityDict = {key: value for key in states for value in cities}
-			myDict = {countries[i] : {key:value} for i, (key,value) in enamurate(state_cityDict.items())}
+			myDict = {countries[i] : {key:value} for i, (key,value) in enumurate(state_cityDict.items())}
 			print(myDict)
 			print('''
 	1. Do you want to Delete countries
