437b3a3ad33c9abdef8be913aaa14f2da14ad871
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-01-13 18:43:14 +0530

    Update exeption_handling.py

--- exeption_handling.py
+++ exeption_handling.py
@@ -1,158 +1,223 @@
+countries = []
+states = []
+cities = []
+
 def add_country():
-	num_countries = int(input("How many coutries you want to add : "))
-	for i in range(num_countries):
-		while (True):
-			country = input("Enter name of country : ")
-			if country.isalpha():
-				break 
-			else:
-				print("\n Invalid input")
-				continue 
-		countries.append(country)
+    num_countries = int(input("How many coutries you want to add : "))
+    for _ in range(num_countries):
+        while True:
+            country = input("Enter the name of country : ").strip()
+            if not country.isalpha():
+                print("\nInvalid input, please enter a valid country name.")
+                continue
+            if country in countries:
+                print(f"\n{country} is already added.")
+            else:
+                countries.append(country)
+            break
 
 
 def update_country():
-    old_name = input("Enter a name of country that you want to update : ")
-    new_name = input("Enter a name of country that you want to add    : ")
-    for i in range(len(countries)):
-        if countries[i] == old_name:
-            countries[i] == new_name
+    old_name = input("Enter a name of country that you want to update : ").strip()
+    
+    if old_name not in countries:
+        print(f"{old_name} does not exist in the country list.")
+        return
+    new_name = input("Enter a name of country that you want to add    : ").strip()
+    if new_name in countries:
+        print(f"{new_name} already exists. Update not perform")
+    else:
+        countries[countries.index(old_name)] = new_name
+        print(f"{old_name} updated to {new_name} successfully!")
 
 
 def remove_country():
-    rm_country_name = input("Enter a country name that you want to remove : ")
-    countries.remove(rm_country_name)
+    country = input("Enter the name of the country you want to remove: ").strip()
+    if country not in countries:
+        print(f"{country} dose not exist in the country list")
+        return
+    countries.remove(country)
+    print(f"{country} removed successfully!")
 
 
 def add_state():
-	for i in range(len(countries)):
-		print(f"Enter {countries[i]}'s states")
-		num_states = int(input("How many states do you want to add in it : "))
-		for i in range(num_states):
-			while (True):
-				state = input("Enter name of state : ")
-				if state.isalpha():
-					break 
-				else:
-					print("\n Invalid input")
-					continue 
-			states.append(state)
+    if not countries:
+        print("\nNo countries available. Add countries first.")
+        return
+    print("Available countries: ", ", ".join(countries))
+    country = input("Enter the name of country to add states: ").strip()
+    if country not in countries:
+        print(f"{country} dose not exist. Please add the country first")
+        return
+    
+    print(f"Enter {countries}'s states")
+    num_states = int(input("How many states do you want to add in it : "))
+    for _ in range(num_states):
+        while True:
+            state = input("Enter the name of country : ").strip()
+            if not state.isalpha():
+                print("\nInvalid input, please enter a valid state name.")
+                continue
+            if state in states:
+                print(f"\n{state} is already added.")
+            else:
+                countries.append(state)
+            break
 
 
 def update_state():
-    old_name = input("Enter a name of state that you want to update : ")
-    new_name = input("Enter a name of state that you want to add    : ")
-    for i in range(len(states)):
-        if states[i] == old_name:
-            states[i] == new_name
+    old_name = input("Enter a name of state that you want to update : ").strip()
+    
+    if old_name not in states:
+        print(f"{old_name} does not exist in the state list.")
+        return
+    new_name = input("Enter a name of state that you want to add    : ").strip()
+    if new_name in states:
+        print(f"{new_name} already exists. Update not perform")
+    else:
+        states[states.index(old_name)] = new_name
+        print(f"{old_name} updated to {new_name} successfully!")
 
 
 def remove_state():
-    rm_state_name = input("Enter a state name that you want to remove : ")
-    states.remove(rm_state_name)
+    state = input("Enter the name of the state you want to remove: ").strip()
+    if state not in states:
+        print(f"{state} dose not exist in the state list")
+        return
+    countries.remove(state)
+    print(f"{state} removed successfully!")
 
 
 def add_city():
-	for i in range(len(states)):\
-	print(f"Enter {states[i]}'s cities")
-	num_cities = int(input("How many city do you want to add in it : "))
-	for i in range(num_cities):
-		while (True):
-			city = input("Enter name of city : ")
-			if citty.isalpha():
-				break 
-			else:
-				continue 
-		cities.append(city)
+    if not states:
+        print("\nNo state available. Add countries first.")
+        return
+    print("Available states: ", ", ".join(countries))
+    state = input("Enter the name of state to add cities: ").strip()
+    if state not in countries:
+        print(f"{state} dose not exist. Please add the country first")
+        return
+
+    print(f"Enter {state}'s states")
+    num_states = int(input("How many states do you want to add in it : "))
+    for _ in range(num_states):
+        while (True):
+            city = input("Enter name of city : ").strip()
+            if not city.isalpha():
+                print("\n Invalid input, please enter a valid city name...")
+                continue
+            if city in cities:
+                print(f"\n{city} is already added.")
+            else:
+                states.append(city)
+                print(f"{city} added successfully!")
+            break
 
 
 def update_city():
-    old_name = input("Enter a name of city that you want to update : ")
-    new_name = input("Enter a name of city that you want to add    : ")
-    for i in range(len(cities)):
-        if cities[i] == old_name:
-            cities[i] == new_name
+    old_name = input("Enter a name of city that you want to update : ").strip()
+    
+    if old_name not in cities:
+        print(f"{old_name} does not exist in the city list.")
+        return
+    new_name = input("Enter a name of city that you want to add    : ").strip()
+    if new_name in states:
+        print(f"{new_name} already exists. Update not perform")
+    else:
+        cities[cities.index(old_name)] = new_name
+        print(f"{old_name} updated to {new_name} successfully!")
 
 
 def remove_city():
-    rm_city_name = input("Enter a city name that you want to remove : ")
-    cities.remove(rm_city_name)
+    city = input("Enter the name of the city you want to remove: ").strip()
+    if city not in cities:
+        print(f"{city} dose not exist in the city list")
+        return
+    countries.remove(city)
+    print(f"{city} removed successfully!")
 
 
-while (True): 
-	countries = []
-	states = []
-	cities = []
+def print_all_date():
+    data = {
+		country : {
+			state : [city for city in cities if city.startswith(state[0])]
+   			for state in states if state.startswith(country[0])
+		}
+		for country in countries
+	}
+    print("\nCurrent Data: ")
+    print(data)
+
+
+while (True):
 	print('''
     1. Add
     2. Update
     3. Delete
-    4. Exit\n''')
+    4. Print all Data
+    5. Exit\n''')
 	opt1 = int(input("Select any option what do you want(1 to 4) : "))
 	if opt1 == 1:
 		while (True):
 			print('''
-    	    1. Do you want to add countries
-    	    2. Do you want to add states
-    	    3. Do you want to add cities
-    	    3. Exit\n''')
+    1. Do you want to add countries
+	2. Do you want to add states
+	3. Do you want to add cities
+	4. Exit\n''')
 			opt2 = int(input("Select any option what do you want(1 to 4) : "))
-			if opt2 == 1: 
+			if opt2 == 1:
 				add_country()
-				continue 
+				continue
 			elif opt2 == 2:
 				add_state()
-				continue 
+				continue
 			elif opt2 == 3:
 				add_city()
-				continue 
+				continue
 			else:
 				break
 	elif opt1 == 2:
 		while (True):
-			state_cityDict = {key: value for key in states for value in cities}
-			myDict = {key: value for key in countries for value in state_cityDict}
-			print(myDict)
 			print('''
-    	    1. Do you want to Update countries
-    	    2. Do you want to Update states
-    	    3. Do you want to Update cities
-    	    3. Exit\n''')
+	1. Do you want to Update countries
+	2. Do you want to Update states
+	3. Do you want to Update cities
+	4. Exit\n''')
 			opt3 = int(input("Select any option what do you want(1 to 4) : "))
 			if opt3 == 1:
 				update_country()
-				continue 
+				continue
 			elif opt3 == 2:
 				update_state()
-				continue 
+				continue
 			elif opt3 == 3:
 				update_state()
-				continue 
+				continue
 			else:
 				break
 	elif opt1 == 3:
 		while (True):
-			state_cityDict = {key: value for key in states for value in cities}
-			myDict = {key: value for key in countries for value in state_cityDict}
-			print(myDict)
 			print('''
-    	    1. Do you want to Delete countries
-    	    2. Do you want to Delete states
-    	    3. Do you want to Delete cities
-    	    3. Exit\n''')
+	1. Do you want to Delete countries
+	2. Do you want to Delete states
+	3. Do you want to Delete cities
+	4. Exit\n''')
 			opt4 = int(input("Select any option what do you want(1 to 4) : "))
 			if opt4 == 1:
 				remove_country()
-				continue 
+				continue
 			elif opt4 == 2:
 				remove_state()
 				continue
 			elif opt4 == 3:
 				remove_state()
-				continue 
+				continue
 			else:
 				break
 	elif opt1 == 4:
+		print_all_date()
+	elif opt1 == 5:
+		print("Exiting the program...")
 		break
 	else:
-		continue 
+		continue
